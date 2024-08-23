da157ee174278b4ba91cb86c21665a9d6aaf247d
Yvelt <pro.yvelt@gmail.com>
2024-08-23 23:20:34 +0200

    Update markers.lua

--- YveltMarkers/client/markers.lua
+++ YveltMarkers/client/markers.lua
@@ -8,11 +8,12 @@ CreateThread(function()
         local spam = false
         local pos = GetEntityCoords(PlayerPedId())
         for k, v in pairs(Yvelt.markers) do
-            if #(pos - v.pos) < v.displayDistance then
+            local dist = #(pos - v.pos)
+            if dist < v.displayDistance then
                 if v.showCondition() then
                     spam = true
                     DrawMarker(v.marker.type, v.pos, 0, 0, 0, v.marker.rot[1], v.marker.rot[2], v.marker.rot[3], v.marker.scale[1], v.marker.scale[2], v.marker.scale[3], v.marker.rgba[1], v.marker.rgba[2], v.marker.rgba[3], v.marker.rgba[4], v.marker.bobUpAndDown, v.marker.faceCamera, 0, v.marker.rotate, 0, 0, 0)
-                    if #(pos - v.pos) < v.interactDistance then
+                    if dist < v.interactDistance then
                         showHelpText(v.helpMsg)
                         if IsControlJustPressed(0, v.interactKey) then
                             CreateThread(function()
